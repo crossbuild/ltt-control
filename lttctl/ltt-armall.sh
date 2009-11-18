1606599b9904eca166d9c30268d79b9641245a82
Benjamin Poirier <benjamin.poirier@polymtl.ca>
2009-11-18 10:49:18 -0500

    Remove commented code
    No need to keep old cruft there! It's in the source control.
    
    Signed-off-by: Benjamin Poirier <benjamin.poirier@polymtl.ca>
    Cc: Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- lttctl/ltt-armall.sh
+++ lttctl/ltt-armall.sh
@@ -1,6 +1,3 @@
-#excluding core markers (already connected)
-#excluding locking markers (high traffic)
-
 DEBUGFSROOT=$(grep ^debugfs /proc/mounts | head -1 | awk '{print $2}')
 MARKERSROOT=${DEBUGFSROOT}/ltt/markers
 
@@ -22,26 +19,3 @@ for c in ${MARKERSROOT}/*; do
 		;;
 	esac
 done
-
-
-# Connect the interesting high-speed markers to the marker tap.
-# Markers starting with "tap_" are considered high-speed.
-#echo Connecting high-rate markers to tap
-#MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u |grep ^tap_`
-#
-##Uncomment the following to also record lockdep events.
-##MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -e ^tap_ -e ^lockdep`
-#
-#IFS=${N}
-#for a in $MARKERS; do
-#	echo Connecting $a
-#
-#	#redirect markers carrying state information to dedicated channels
-#	case $a in
-#	*)
-#		CHANNEL=
-#		;;
-#	esac
-#
-#	echo "connect $a ltt_tap_marker" > /proc/ltt
-#done
--- lttctl/ltt-disarmall.sh
+++ lttctl/ltt-disarmall.sh
@@ -1,6 +1,3 @@
-#excluding core markers (already connected)
-#excluding locking markers (high traffic)
-
 DEBUGFSROOT=$(grep ^debugfs /proc/mounts | head -1 | awk '{print $2}')
 MARKERSROOT=${DEBUGFSROOT}/ltt/markers
 
@@ -22,17 +19,3 @@ for c in ${MARKERSROOT}/*; do
 		;;
 	esac
 done
-
-## Markers starting with "tap_" are considered high-speed.
-#echo Disconnecting high-rate markers to tap
-#MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2 " " $4}'|sort -u |grep ^tap`
-#
-##Uncomment the following to also stop recording lockdep events.
-##MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -e ^tap_ -e ^lockdep`
-#
-#IFS=${N}
-#for a in $MARKERS; do
-#	echo Disconnecting $a
-#
-#	echo "disconnect $a ltt_tap_marker" > /proc/ltt
-#done
